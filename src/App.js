0c3a0fa4e58dbd364aee3fe39c7bdc2c1cea25cd
dpodboj <damir.podbojec@gmail.com>
2017-11-13 10:30:11 +0100

    Handled submit and change

--- src/App.js
+++ src/App.js
@@ -7,27 +7,30 @@ class App extends Component {
     super(props);
     this.state = {
       data: null,
-      newdata: '',
+      newData: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     database.ref().on('value', (snapshot) => {
       this.setState({
-        data: snapshot.val()
+        data: snapshot.val(),
       });
     });
   }
 
   handleChange(event) {
     const newData = event.target.value;
-    this.setState = ({
-      newdata: newData,
+    this.setState({
+      newData,
     });
   }
 
   handleSubmit(event) {
     event.preventDefault();
+    database.ref().child('New data').set(this.state.newData);
   }
 
   render() {
@@ -39,8 +42,8 @@ class App extends Component {
         <pre className="App--data">
           { JSON.stringify(this.state.data, null, 2) }
         </pre>
-        <form className="App--form" onSubmit={this.handleSubmit} />
-          <input type="text" onChange={this.handleChange} />
+        <form className="App--form" onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.newData} onChange={this.handleChange} />
           <input type="submit" />
         </form>
       </div>
